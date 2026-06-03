80d4da9fddd11100a5843b39a51ad8a85048911c
Surya <suryavk42@gmail.com>
2026-06-03 23:40:29 +0530

    day1

--- src/test/java/AddPlace.java
+++ src/test/java/AddPlace.java
@@ -1,6 +1,9 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import jsonResources.Payloads;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class AddPlace {
 
@@ -8,24 +11,15 @@ public class AddPlace {
 
         RestAssured.baseURI = "https://rahulshettyacademy.com";
 
-        given().log().all().header("Content-Type","application/json").queryParam("key","qaclick123")
-                .body("{\n" +
-                        "  \"location\": {\n" +
-                        "    \"lat\": -38.383494,\n" +
-                        "    \"lng\": 33.427362\n" +
-                        "  },\n" +
-                        "  \"accuracy\": 50,\n" +
-                        "  \"name\": \"Home one\",\n" +
-                        "  \"phone_number\": \"(+91) 983 893 3937\",\n" +
-                        "  \"address\": \"29, side layout, chennai 13\",\n" +
-                        "  \"types\": [\n" +
-                        "    \"toe park\",\n" +
-                        "    \"house\"\n" +
-                        "  ],\n" +
-                        "  \"website\": \"http://google.com\",\n" +
-                        "  \"language\": \"French-IN\"\n" +
-                        "}\n")
+        String response = given().log().all().header("Content-Type","application/json").queryParam("key","qaclick123")
+                .body(Payloads.addPlace())
                 .when().post("maps/api/place/add/json")
-                .then().log().all().statusCode(200);
+                .then().log().all().statusCode(200).body("scope",equalTo("APP"))
+                .header("Server","Apache/2.4.52 (Ubuntu)").extract().response().asString();
+
+        JsonPath jsonPath = new JsonPath(response);
+        String placeId = jsonPath.get("place_id");
+        System.out.println("Extracted place ID: " + placeId);
+
     }
 }
---
+++ src/test/java/jsonResources/Payloads.java
@@ -0,0 +1,24 @@
+package jsonResources;
+
+public class Payloads {
+
+    public static String addPlace(){
+
+        return "{\n" +
+                "  \"location\": {\n" +
+                "    \"lat\": -38.383494,\n" +
+                "    \"lng\": 33.427362\n" +
+                "  },\n" +
+                "  \"accuracy\": 50,\n" +
+                "  \"name\": \"Home one\",\n" +
+                "  \"phone_number\": \"(+91) 983 893 3937\",\n" +
+                "  \"address\": \"29, side layout, chennai 13\",\n" +
+                "  \"types\": [\n" +
+                "    \"toe park\",\n" +
+                "    \"house\"\n" +
+                "  ],\n" +
+                "  \"website\": \"http://google.com\",\n" +
+                "  \"language\": \"French-IN\"\n" +
+                "}\n";
+    }
+}
